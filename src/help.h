c0d9c4c5628f7f675b4d8f1a2db90b333caf194a
XDragonSl <almdragonsl@gmail.com>
2020-07-10 15:07:00 +0300

    Added displaying current user, version & help

---
+++ src/help.h
@@ -0,0 +1,15 @@
+#ifndef HELP_H_
+#define HELP_H_
+
+#include <string>
+
+using namespace std;
+
+const string VERSION(".0");
+
+const string H_COMMON("Git Switcher\nVersion " + VERSION + "\n\ngsw <option>");
+const string H_CURRENT("\t-c, --current:\tDisplay current user email");
+const string H_VERSION("\t-v, --version:\tDisplay installed version");
+const string H_HELP("\t-h, --help:\tDisplay help");
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
-#include <stdio.h>
+#include <string>
+#include <iostream>
+#include "Options/Options.h"
+#include "help.h"
 
-int main() {
-	printf("Hello, World!");
-	return 0;
+using namespace std;
+
+const string WRONG_OPTS_MESSAGE("Wrong options!\nRun gsw --help for usage information");
+const string CURRENT_GIT_CMD("git config --get user.email");
+
+void current();
+void version();
+void help();
+
+int main(int argc, char **argv) {
+    Options opts(argc, argv);
+    if (opts.isExists("-c") || opts.isExists("--current")) {
+        current();
+    } else if (opts.isExists("-h") || opts.isExists("--help")) {
+        help();
+    } else if (opts.isExists("-v") || opts.isExists("--version")) {
+        version();
+    } else {
+        cout << WRONG_OPTS_MESSAGE;
+    }
+    return 0;
+}
+
+void current() {
+    system(CURRENT_GIT_CMD.c_str());
+}
+
+void version() {
+    cout << VERSION;
+}
+
+void help() {
+    cout << H_COMMON + "\n";
+    cout << "Options:\n" << H_CURRENT + "\n" << H_VERSION + "\n" << H_HELP + "\n\n";
 }
