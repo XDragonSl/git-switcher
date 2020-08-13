574bd33debe24dec24086a59cb41a3db4788c81d
XDragonSl <almdragonsl@gmail.com>
2020-08-13 15:51:57 +0300

    Store updated

--- src/Store/Store.cpp
+++ src/Store/Store.cpp
@@ -1,12 +1,31 @@
 #include "Store.h"
-#include <iostream>
-
-using namespace std;
+#include <sstream>
 
 Store::Store() {
-    store.open(STORE_LOCATION + STORE_NAME);
+    store.open(STORE_LOCATION + STORE_NAME, ios::in);
+    if (store.is_open()) {
+        string profileString;
+        string name;
+        string email;
+        while (getline(store, profileString)) {
+            istringstream profileStream(profileString);
+            getline(profileStream, name, Profile::SEPARATOR);
+            getline(profileStream, email, Profile::SEPARATOR);
+            Profile loadedProfile(name, email);
+            allProfiles.push_back(loadedProfile);
+        }
+    } else {
+        store.clear();
+    }
+    store.close();
 }
 
 Store::~Store() {
-    store.close();
+    if (isChanged) {
+        store.open(STORE_LOCATION + STORE_NAME, ios::out);
+        for(Profile profile : allProfiles) {
+            store << profile.toString() << endl;
+        }
+        store.close();
+    }
 }
--- src/Store/Store.h
+++ src/Store/Store.h
@@ -3,21 +3,24 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
+#include <vector>
+#include "../Profile/Profile.h"
 
 using namespace std;
-
 
 #ifdef _WIN32
     const string STORE_LOCATION(string(getenv("USERPROFILE")) + "\\");
 #else
-    const string STORE_LOCATION(string(getenv("HOME")) + "\\");
+    const string STORE_LOCATION(string(getenv("HOME")) + "/");
 #endif
 
 const string STORE_NAME(".gsw");
 
 class Store {
     private:
-        ofstream store;
+        fstream store;
+        vector<Profile> allProfiles;
+        bool isChanged = false;
     public:
         Store();
         ~Store();
